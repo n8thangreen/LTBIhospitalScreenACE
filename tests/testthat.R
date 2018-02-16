37818e8fd7548abb0cd6b4580a1e639994914b06
Dr Nathan Green <nathan.green@imperial.ac.uk>
2018-02-16 12:59:22 +0000

    add test folder

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(LTBIhospitalScreenACE)
+
+test_check("LTBIhospitalScreenACE")
---
+++ tests/testthat/test-decision_tree_ACE.R
@@ -0,0 +1,14 @@
+context("decision_tree_ACE")
+
+test_that("boundary scenario values", {
+  # to debug
+  # xx <- decision_tree_ACE(parameters = scenario_parameters[[1]],
+  #                         N.mc = 100,
+  #                         cost_dectree = "osNode_cost.Rds",
+  #                         health_dectree = "osNode_cost.Rds")
+  #
+  # xx <- decision_tree_ACE(parameters = data.frame("val_type" = NA),
+  #                         N.mc = 100,
+  #                         cost_dectree = "osNode_cost.Rds",
+  #                         health_dectree = "osNode_cost.Rds")
+})
